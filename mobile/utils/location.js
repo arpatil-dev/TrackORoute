03ae6b5a459eabb49ce1c2372d99eef590c9bd5e
arpatil-dev <aryanrpatil2023@gmail.com>
2025-08-30 18:21:35 +0530

    refactor(location.js): Code Formatting and refactoring done

--- mobile/utils/location.js
+++ mobile/utils/location.js
@@ -1,19 +1,31 @@
 import * as Location from 'expo-location';
-
+/* expo-location is used for accessing device location services */
+/* Request location permissions from the user */
 export async function requestLocationPermissions() {
+  /* Request foreground location permissions */
   let { status } = await Location.requestForegroundPermissionsAsync();
+  /* If not granted, throw an error */
   if (status !== 'granted') {
     throw new Error('Permission to access location was denied');
   }
+  /* If granted, return true*/
   return true;
 }
 
+/* Get the current location of the device */
 export async function getCurrentLocation() {
   return await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
 }
 
+/* Start watching location changes with specified interval and distance 
+  Calls the provided callback with location updates 
+  Default interval is 5000ms and distance is 2 meters 
+  Throttles updates to avoid excessive calls 
+  Only calls callback if location changes significantly 
+  Returns the subscription object to allow stopping updates */
 export async function startLocationUpdates(callback, interval = 5000, distance = 2) {
   let lastSent = null;
+  /* Start watching position with given accuracy, time interval, and distance interval */
   return Location.watchPositionAsync(
     {
       accuracy: Location.Accuracy.High,
@@ -33,7 +45,9 @@ export async function startLocationUpdates(callback, interval = 5000, distance =
   );
 }
 
+/* Stop watching location changes by removing the subscription */
 export async function stopLocationUpdates(subscription) {
+  /* Remove the subscription to stop receiving location updates */
   if (subscription) {
     subscription.remove();
   }
